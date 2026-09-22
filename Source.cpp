1f74bb54b99ef8ad0a351f8fedfd1a6aa4f8504f
meghapanchani <meghapanchani26@gmail.com>
2026-09-22 14:43:17 -0230

    Successfully reading the student data file

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct STUDENT_DATA
+{
+    string firstName;
+    string lastName;
+};
+
 int main()
 {
-    return 1;
+    vector<STUDENT_DATA> students;
+
+    ifstream inputFile("StudentData.txt");
+
+    if (!inputFile.is_open())
+    {
+        cout << "Cant open the StudentData.txt" << endl;
+        return 1;
+    }
+
+    string line;
+
+    while (getline(inputFile, line))
+    {
+        size_t commaPosition = line.find(',');
+
+        STUDENT_DATA student;
+
+        student.firstName = line.substr(0, commaPosition);
+        student.lastName = line.substr(commaPosition + 1);
+
+        students.push_back(student);
+    }
+
+    inputFile.close();
+
+    return 0;
 }
